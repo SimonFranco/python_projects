502432e678ad3a1c8e43743de105dec389f2a753
Simon Franco <simonfranco0@gmail.com>
2023-01-02 20:46:46 -0800

    initial commit

---
+++ functions_practice.py
@@ -0,0 +1,24 @@
+def hello():
+  print("hello")
+
+hello()
+
+def pack(a,b,c):
+    return [a,b,c]
+
+print(pack("a","b","c"))
+
+
+def eat_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
+
+eat_lunch([])
+eat_lunch(["cookie"])
+eat_lunch(["muffin","soda","sandwich","cookie"])
